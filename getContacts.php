9c24ab4283e1883d48bd3b932f47b7f9b15e7ef3
danish505 <danishghulamali@gmail.com>
2021-02-16 14:07:44 -0600

    Update getContacts.php

--- getContacts.php
+++ getContacts.php
@@ -3,7 +3,7 @@
 
 include 'vendor/autoload.php'; 
 
-$hubspot = SevenShores\Hubspot\Factory::create('fbb8b9ee-fd9d-4672-807c-91386dda8abc');
+$hubspot = SevenShores\Hubspot\Factory::create('API_KEY');
 
 $response = $hubspot->contacts()->all();
 
