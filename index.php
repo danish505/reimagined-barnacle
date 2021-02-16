3b1ee49613b26898623eec5401b73bb923b78270
danish505 <danishghulamali@gmail.com>
2021-02-16 14:06:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 include 'vendor/autoload.php'; 
 
-$hubspot = SevenShores\Hubspot\Factory::create('fbb8b9ee-fd9d-4672-807c-91386dda8abc');
+$hubspot = SevenShores\Hubspot\Factory::create('api_key');
 
 
 
